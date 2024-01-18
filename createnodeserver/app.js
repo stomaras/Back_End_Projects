ffe96b803154c045d8ec6bf916888138c3a782f0
stomaras <sptoma724@gmail.com>
2024-01-18 16:46:05 +0200

    Section 6 Sharing Data Across Requests and Users

--- createnodeserver/app.js
+++ createnodeserver/app.js
@@ -30,7 +30,7 @@ const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // this register some middleware call next in the end , before will do body parsing request 
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 // use handle all http methods
 app.use((req,res,next) => {
