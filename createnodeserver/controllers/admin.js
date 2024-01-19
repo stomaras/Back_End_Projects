6dab1865a8a85968ab786aa1df426a7bfb3c6c0e
stomaras <sptoma724@gmail.com>
2024-01-19 15:41:26 +0200

    Section 8 nodejs done

--- createnodeserver/controllers/admin.js
+++ createnodeserver/controllers/admin.js
@@ -12,7 +12,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req,res,next) => {
     const title = req.body.title;
-    const product = new Product(title);
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/')
 }
