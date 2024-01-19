1d6090ea18b49d02d900b45f74a484615d4558e7
stomaras <sptoma724@gmail.com>
2024-01-19 14:01:59 +0200

    Registering the Routes

--- createnodeserver/controllers/admin.js
+++ createnodeserver/controllers/admin.js
@@ -17,16 +17,12 @@ exports.postAddProduct = (req,res,next) => {
     res.redirect('/')
 }
 
-exports.getProducts = (req,res,next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', {
-            prods:products, 
-            pageTitle:'Shop', 
-            path:'/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS:true,
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods:products,
+            pageTitle:'Admin Products',
+            path:'/admin/products'
         });
     });
-    
-}
+};
--- createnodeserver/routes/admin.js
+++ createnodeserver/routes/admin.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
