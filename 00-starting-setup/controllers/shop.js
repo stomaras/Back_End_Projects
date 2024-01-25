e7d56e323ab2174a5a0a463af344e23d061d0897
stomaras <sptoma724@gmail.com>
2024-01-25 22:35:30 +0200

    create associations

--- 00-starting-setup/controllers/shop.js
+++ 00-starting-setup/controllers/shop.js
@@ -2,25 +2,27 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
+    Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/'
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) => {
+  Product.findByPk(prodId)
+  .then((product) => {
     console.log(product);
     res.render('shop/product-detail', {
-      product: product[0],
-      pageTitle: product[0].title,
+      product: product,
+      pageTitle: product.title,
       path: '/products'
     });
   })  
@@ -28,17 +30,19 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/'
       });
     })
-    .catch(err => console.log(err));
-  
-};
+    .catch((err) => {
+      console.log(err);
+    });
+
+  };
 
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
