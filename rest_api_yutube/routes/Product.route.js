9e5e0edf03066347da4ce589af8936e1412ca73c
stomaras <sptoma724@gmail.com>
2024-01-27 19:44:10 +0200

    CRUD Product Done

--- rest_api_yutube/routes/Product.route.js
+++ rest_api_yutube/routes/Product.route.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product.model');
 
-router.get('/', (req, res, next) => {
-    next(new Error("cannot get a list of all products"))
-    // res.send('gettin')
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await Product.find({},{__v:0});
+        // const results = await Product.find({}, {name:1, 
+        //     price:1,
+        //     _id:0
+        // });
+        // const results = await Product.find({price: 345}, {});
+        res.send(results);
+    }catch(error){
+
+    }
 });
 
 // Create a new product
 router.post('/', async (req, res, next) => {
-
     try {
         const product = new Product(req.body);
         const result = await product.save();
@@ -17,31 +25,42 @@ router.post('/', async (req, res, next) => {
     }catch(err){
         console.log(err.message);
     }
-    // const product = new Product({
-    //     name:req.body.name,
-    //     price: req.body.price
-    // })
-    // product.save()
-    // .then(result => {
-    //     console.log(result);
-    //     res.send(result);
-    // })
-    // .catch(err => {
-    //     console.log(err.message);
-    // })
-
 });
 
-router.get('/:id', (req,res,next) => {
-    res.send('getting a single product')
+// Get a product by id
+router.get('/:id', async (req,res,next) => {
+    const id = req.params.id;
+    try {
+        const product = await Product.findById(id);
+        res.send(product)
+    }catch(error){
+        console.log(error.message);
+    }
 })
 
-router.patch('/:id', (req,res,next) => {
-    res.send('updating a single product')
+// Update a product by id
+router.patch('/:id', async(req,res,next) => {
+    try {
+        const id = req.params.id
+        const updates = req.body
+        const options = {new: true};
+
+        const result = await Product.findByIdAndUpdate(id,updates,options)
+        res.send(result);
+    }catch(error){
+        console.log(error.message);
+    }
 })
 
-router.delete('/:id', (req,res,next) => {
-    res.send('deleting a single product')
+// Delete a product by id
+router.delete('/:id', async(req,res,next) => {
+    const id = req.params.id;
+    try {
+        const result = await Product.findByIdAndDelete(id);
+        res.send(result);
+    }catch(error){
+        console.log(error.message);
+    }
 })
 
 module.exports = router;
