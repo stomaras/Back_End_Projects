ab5934ef48c37aa81c0af1c1b68593804edfacb6
stomaras <sptoma724@gmail.com>
2024-01-29 23:34:55 +0200

    server validation wip

--- RESTAPI_PRACTICAL/backend/controllers/feed.js
+++ RESTAPI_PRACTICAL/backend/controllers/feed.js
@@ -1,28 +1,31 @@
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts:[
-            {
-                _id:'1',
-                title:'First Post', 
-                content:'This is the first stop', 
-                imageUrl: 'images/image.jpg',
-                creator: {
-                    name:'Tom'
-                },
-                createdAt: new Date(),
-            }
-        ]
-    });
+  res.status(200).json({
+    posts: [
+      { 
+        _id: 1,
+        title: 'First Post', 
+        content: 'This is the first post!', 
+        imageUrl:'images/lake.jpg',
+        creator: {
+          name:'Tom'
+        },
+        createdAt: new Date() 
+      }]
+  });
 };
 
 exports.createPost = (req, res, next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    console.log(title,content);
-    // create post in db
-    // 201 indicates that we created a resource
-    res.status(201).json({
-        message:'Post created successfully',
-        post: {id: new Date().toISOString(), title: title, content:content}
-    });
-}
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { 
+      _id: new Date().toISOString(), 
+      title: title, 
+      content: content,
+      creator: {name:'Max'},
+      createdAt: new Date()
+    }
+  });
+};
