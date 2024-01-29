78e27b2b4352c127ae4499da36e409f70770e858
stomaras <sptoma724@gmail.com>
2024-01-29 23:55:15 +0200

    connect to mongo with cluster

--- RESTAPI_PRACTICAL/backend/app.js
+++ RESTAPI_PRACTICAL/backend/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+const uri = 'mongodb+srv://udemy:udemy@cluster0.clb7qvm.mongodb.net/?retryWrites=true&w=majority'
 
+async function connect() {
+    try {
+        await mongoose.connect(uri)
+        console.log("connected to monodb");
+    }catch(error){
+        console.log(error);
+    }
+}
+
+connect();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
