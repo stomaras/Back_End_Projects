d98255012d1143e0b6d4fab32a7b6782687421ad
stomaras <sptoma724@gmail.com>
2024-02-23 18:26:27 +0200

    Viewsets and Routers

--- DjangoIMDV/watchmate/watchlist_app/api/urls.py
+++ DjangoIMDV/watchmate/watchlist_app/api/urls.py
@@ -1,18 +1,28 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail,StreamPlatformVS
+from watchlist_app.api.views import ReviewCreate
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
 
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    
+    path('', include(router.urls)),
+    # path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    # path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
     
     path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='stream-detail'),
     
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
     # path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
--- DjangoIMDV/watchmate/watchlist_app/api/views.py
+++ DjangoIMDV/watchmate/watchlist_app/api/views.py
@@ -6,17 +6,43 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = WatchList.objects.get(pk=pk)
+        serializer.save(watchlist=watchlist)    
+
 
 class ReviewList(generics.ListCreateAPIView):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
     
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     
-
-
+# for simple requirements use ViewSet and router will help us 
+# for complex requirements use GenericViewSet
+class StreamPlatformVS(viewsets.ViewSet):
+    
+    def list(self, request):
+        queryset = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        watchlist = get_object_or_404(queryset, pk=pk)
+        serializer = StreamPlatformSerializer(watchlist)
+        return Response(serializer.data)
 
 class StreamPlatformAV(APIView):
     
