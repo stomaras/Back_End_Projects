8fb77f54a116dc1218bb48ccecf5565a84592a7b
stomaras <sptoma724@gmail.com>
2024-02-24 19:09:25 +0200

    day 33 almost done

---
+++ python/Day33/main.py
@@ -0,0 +1,34 @@
+import requests 
+from datetime import datetime
+
+MY_LAT = 51.5072
+MY_LONG = -0.1278
+
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted":0,
+}   
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data['results']['sunrise'].split("T")[1].split(":")[0]
+sunset = data['results']['sunset'].split("T")[1].split(":")[0]
+
+
+print(sunrise)
+print(sunset)
