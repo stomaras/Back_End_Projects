26573894799afba99881e83995600a98f96d1db6
stomaras <sptoma724@gmail.com>
2024-02-24 16:19:06 +0200

    ModelViewSets

--- DjangoIMDV/watchmate/watchlist_app/api/urls.py
+++ DjangoIMDV/watchmate/watchlist_app/api/urls.py
@@ -8,7 +8,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('stream', StreamPlatformVS, basename='streamplatform')
 
-
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
--- DjangoIMDV/watchmate/watchlist_app/api/views.py
+++ DjangoIMDV/watchmate/watchlist_app/api/views.py
@@ -31,18 +31,34 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     
 # for simple requirements use ViewSet and router will help us 
 # for complex requirements use GenericViewSet
-class StreamPlatformVS(viewsets.ViewSet):
+
+class StreamPlatformVS(viewsets.ReadOnlyModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
     
-    def list(self, request):
-        queryset = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(queryset, many=True)
-        return Response(serializer.data)
+
+# class StreamPlatformVS(viewsets.ViewSet):
     
-    def retrieve(self, request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+#     def list(self, request):
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
+    
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+    
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+    
+    
 
 class StreamPlatformAV(APIView):
     
