107339d16b0219c5e6f3404ee8edc19ad69929d4
stomaras <sptoma724@gmail.com>
2024-03-29 11:34:12 +0200

    How to update Models in Django

--- DjangoFullCourseYutube/watchmate/watchlist_app/api/serializers.py
+++ DjangoFullCourseYutube/watchmate/watchlist_app/api/serializers.py
@@ -1,19 +1,50 @@
 from rest_framework import serializers
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class StreamPlatformSerializer(serializers.ModelSerializer):
     
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    class Meta:
+        model = StreamPlatform
+        fields = '__all__'    
+
+
+class WatchListSerializer(serializers.ModelSerializer):
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    class Meta:
+        model = WatchList
+        fields = '__all__'
+        
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("Name is too short")
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+    
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+    
+#     # object based validation
+#     def validate(self,data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Title and Description should be deifferent.")
+#         else:
+#             return data
+        
+#     # property name validation 
+#     def validate_name(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")
+#         else:
+#             return value
         
--- DjangoFullCourseYutube/watchmate/watchlist_app/api/urls.py
+++ DjangoFullCourseYutube/watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from watchlist_app.api.views import movie_list, movie_details
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAView
+
 urlpatterns = [
-    path('list/', movie_list, name="movie-list"),
-    path('<int:pk>', movie_details, name='movie-details'),
+    path('list/', WatchListAV.as_view(), name="movie-list"),
+    path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
+    path('stream/', StreamPlatformAView.as_view(), name='stream')
 ]
