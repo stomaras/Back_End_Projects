6cfbdfc2c8385b8ba1b2b3d0333af0bac89b8242
stomaras <sptoma724@gmail.com>
2024-04-03 22:05:52 +0300

    Django Reest Framework Yutube GenericAPI View and Mixins

--- DjangoFullCourseYutube/watchmate/watchlist_app/api/urls.py
+++ DjangoFullCourseYutube/watchmate/watchlist_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAView, StreamPlatformDetailAV
+from watchlist_app.api.views import ReviewList, ReviewDetail ,WatchListAV, WatchDetailAV, StreamPlatformAView, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie-list"),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
     path('stream/', StreamPlatformAView.as_view(), name='stream'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
