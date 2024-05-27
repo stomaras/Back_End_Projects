9be37c20c81afcc4a74e2ad028081e74f85545f7
stomaras <sptoma724@gmail.com>
2024-05-27 19:56:38 +0300

    Facade Terminologies

---
+++ DesignPatterns/Udemy/Facade/facade.py
@@ -0,0 +1,11 @@
+# Terminologies
+
+# Facade: The Facade is a simplified interface that provides access to a complex system or subsystem.
+# It provides a single point of entry to the underlying system, and it encapsulates the complexity of the system
+# from the client.
+
+# Complex Subsystem: The Complex Subsystem is a set of classes or components that provide a complex set of functionalities.
+# These classes are typically intependent, and they may have complex relationships with each other. 
+
+# Client: The Client is the object that uses the Facade to interact with the complex subsytem. The client code 
+# does not need to know the details of how the subsystem works, and it simply interacts with the Facade.
